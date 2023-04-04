9392a92060e2ef021f4c7885cb859f85bec4d67e
CHARLESEmm <ccharlesemmanuel@gmail.com>
2023-04-04 19:19:41 +0200

    WIP page home and footer 1.0

--- addbooks.php
+++ addbooks.php
@@ -44,7 +44,7 @@ if (isset($_POST['deconnexion'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="base.css">
+    <link rel="stylesheet" href="addbooks.css">
     <title>Document</title>
 </head>
 <body>
