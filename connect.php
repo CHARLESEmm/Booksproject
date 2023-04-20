ddbfb02014924cee6f30ee757ab01fe0b240a53c
Emmanuelcharles <114005392+CHARLESEmm@users.noreply.github.com>
2023-04-21 00:08:45 +0200

    Delete connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-       $conn = new PDO("mysql:host=lb221694-001.eu.clouddb.ovh.net;dbname=ProjetsSIO2023;charset=utf8;port=35508", "adminSIO", "BTSsio2022", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
-    /*$conn = new PDO('mysql:host=localhost;dbname=books;charset=utf8','root','',array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));*/
-
-?>
