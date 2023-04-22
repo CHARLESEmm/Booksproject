a117eb8f2c31a38d92c4cc2404b49caff9239903
CHARLESEmm <ccharlesemmanuel@gmail.com>
2023-04-23 01:50:13 +0200

    wip partie categorie fini

--- addbooks.php
+++ addbooks.php
@@ -15,6 +15,7 @@ if (isset($_POST['addbooks'])) {
     $summary = $_POST['summary'];
     $details = $_POST['details'];
     $price = $_POST['price'];
+    $categories = $_POST['categories'];
     $link = NULL;
     $poster = NULL;
 
@@ -58,12 +59,29 @@ if (isset($_POST['addbooks'])) {
         // Insertion en base de données
         $stmt = $conn->prepare('INSERT INTO ec_book(title, author, summary, details, price, poster, link) VALUES (?, ?, ?, ?, ?, ?, ?)');
         $stmt->execute(array($title, $author, $summary, $details, $price, $poster, $link));
-        $erreur = 'Votre livre a bien été ajouté.';
+    
+        $book_id = $conn->lastInsertId();
+    
+        if ($book_id > 0) {
+            $stmt = $conn->prepare('INSERT INTO ec_categorie (categorie, book_id) VALUES (?, ?)');
+            foreach((array)$categories as $categorie) {
+                $stmt->execute(array($categorie, $book_id));
+            }
+    
+            $erreur = 'Votre livre a bien été ajouté.';
+        } else {
+            $erreur = 'Formulaire non soumis.';
+        }
+    } else {
+        echo "Erreur lors de l'ajout du livre.";
     }
+    
+    } else {
+        echo 'Formulaire non soumis.';
+    }
+    
 
-} else {
-    $erreur = 'Formulaire non soumis.';
-}
+
 
 
 session_start();
@@ -109,51 +127,45 @@ if (isset($_POST['deconnexion'])) {
         <div class="formulaire">
             <div class="boxcontenairadminprofil">
                 <div class="image_admin"><img src="assets/avatar.png" alt="" srcset=""></div>
-                <div class="adminname"><?php echo isset($_SESSION['adminname']) ? $_SESSION['adminname'] : ''; ?></div>
+                <div class="adminname"><a href="addbooks.php"><?php echo isset($_SESSION['adminname']) ? $_SESSION['adminname'] : ''; ?></a></div>
             </div>
             <div class="titre">
                 <h2>Gestion d'articles</h2>
             </div>
-        <div class="formbox">
-            <form action="" method="post" class="gestionarticlesform" enctype="multipart/form-data">
+            <div class="formbox">
+        <form action="" method="post" class="gestionarticlesform" enctype="multipart/form-data">
             <label for="title">Title</label><br>
             <input type="text" id="sizeinput" name="title">
-             <br>
+            <br>
+            <label for="categories">Categories</label>
+                <select name="categories">
+                <option value="science-fiction">Science Fiction</option>
+                <option value="roman">Roman</option>
+                <option value="historique">Historique</option>
+                </select><br>
             <label for="author">Author</label><br>
             <input type="text" id="sizeinput" name="author"><br>
-            
-            <label for="summary">Summary</label>
-            <br>
-            <textarea  id="story" name="summary"
-                rows="10" cols="33"> </textarea><br>
-            <label for="summary">Details</label>
-            <br>
-            <textarea  id="story" name="details"
-                rows="8" cols="33"> </textarea><br><br>   
-            <label for="price">Price</label>
-            <br>
+            <label for="summary">Summary</label><br>
+            <textarea id="story" name="summary" rows="10" cols="33"></textarea><br>
+            <label for="summary">Details</label><br>
+            <textarea id="story" name="details" rows="8" cols="33"></textarea><br><br>   
+            <label for="price">Price</label><br>
             <input type="text" id="sizeinput" name="price"><br>
-
-            <label for="posteradd">Poster </label><br>
+            <label for="posteradd">Poster</label><br>
             <input type="file" id="sizeinput" name="photo" placeholder="emplacement du ficher/filename.png" accept="image/png,image/jpeg,image/jpg"><br><br>
-           
             <label for="link">link</label>
-            <input type="file" id="sizeinput" name="link" placeholder="emplacement du ficher/title.epub or pdf" accept="application/pdf,application/epub+zip" ><br><br>
+            <input type="file" id="sizeinput" name="link" placeholder="emplacement du ficher/title.epub or pdf" accept="application/pdf,application/epub+zip"><br><br>
             <input type="submit" value="add" id="boutton_add" name="addbooks">
-            
             <div class="messageerror">
-                        <?php
-                            if (isset($erreur))
-                            {
-                                echo '<font color="orange">' .$erreur."</font>";
-                                exit();
-                        
-                            }
-                          
-                        ?>
-                    </div>
-        </form>
-        </div>
+            <?php
+            if (isset($erreur)) {
+                echo '<font color="orange">' .$erreur."</font>";
+                exit();
+            }
+            ?>
+            </div>
+  </form>
+</div>
         
         </div>
 </div>
